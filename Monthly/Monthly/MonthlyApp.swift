f8e97c73988e4be493f12df644249e73ab4e0c03
Batuhan Koçak <batuhankocak@yandex.com>
2022-08-19 01:22:18 +0300

    Add files via upload

---
+++ Monthly/Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Batuhan Kocak on 19.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
